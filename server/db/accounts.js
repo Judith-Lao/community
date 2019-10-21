b2a23fa0a144b3e0dd9554f33847baf4db541ee9
Judith Lao <laojudith2014@gmail.com>
2019-10-21 01:07:18 -0400

    needs actual seed file, example seed enclosed in bin

---
+++ server/db/accounts.js
@@ -0,0 +1,20 @@
+
+'use strict';
+
+const Sequelize = require('sequelize');
+const db = require('./database');
+
+const Account = db.define('account', {
+ name: {
+   type: Sequelize.INTEGER
+ },
+ type: {
+   type: Sequelize.STRING,
+   allowNull: false,
+   validate: {
+     notEmpty: true
+   }
+ }
+});
+
+module.exports = Account;
